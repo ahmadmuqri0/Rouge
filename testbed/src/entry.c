972c703f8edd98043d75d7e9be40238945bac675
ahmadmuqri0 <muqrisuhaimi25@gmai.com>
2024-05-24 16:07:37 +0800

    change window position

--- testbed/src/entry.c
+++ testbed/src/entry.c
@@ -8,7 +8,7 @@
 b8 create_game(game* out_game) {
     // Application configuration.
     out_game->app_config.start_pos_x = 100;
-    out_game->app_config.start_pos_y = 100;
+    out_game->app_config.start_pos_y = 50;
     out_game->app_config.start_width = 1280;
     out_game->app_config.start_height = 720;
     out_game->app_config.name = "Rouge Engine Testbed";
